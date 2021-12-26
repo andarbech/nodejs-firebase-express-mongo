7fe8ca1bd75ebd0f595a03853d37caabaf838f8d
andarbech <andarbech1.com>
2021-12-26 12:31:32 +0100

    missingbackend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home/Home";
+import{BrowserRouter,Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+  </Routes>
+  </BrowserRouter>
+    );
+  }
+  
 export default App;
